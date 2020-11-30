fa985b33164d994417d4a0460bd2a8010f2cbe24
Óscar García <oscarpoblacion@gmail.com>
2020-11-30 19:18:25 +0000

    Inicial code for catalog API

---
+++ app/catalog.py
@@ -0,0 +1,28 @@
+import uuid
+def get_products():
+	fake_response = [{
+		"sku": uuid.uuid4(),
+		"title": "Vanilla icecream",
+		"long_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat semper metus, ac blandit risus viverra eu. Nullam commodo posuere velit, efficitur fringilla turpis posuere a. Vestibulum tempus scelerisque nunc, a efficitur justo hendrerit a. Fusce eu porttitor diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere nec risus nec aliquam. Nulla interdum arcu at sodales vestibulum. Suspendisse velit risus, aliquet vel tristique quis, suscipit sit amet nibh. Vivamus viverra eros odio, a aliquet sapien mollis vel. Duis scelerisque leo nulla, sit amet laoreet mi sodales vitae. Suspendisse vitae consectetur metus, vel imperdiet nulla. Fusce consectetur varius neque, sit amet ornare erat porta ac. Sed ligula lectus, sollicitudin ut pretium quis, maximus eu sapien. Curabitur vitae lacinia urna, sit amet iaculis tellus.",
+		"price_euro": 1.5
+	},{
+		"sku": uuid.uuid4(),
+		"title": "Cola icecream",
+		"long_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat semper metus, ac blandit risus viverra eu. Nullam commodo posuere velit, efficitur fringilla turpis posuere a. Vestibulum tempus scelerisque nunc, a efficitur justo hendrerit a. Fusce eu porttitor diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere nec risus nec aliquam. Nulla interdum arcu at sodales vestibulum. Suspendisse velit risus, aliquet vel tristique quis, suscipit sit amet nibh. Vivamus viverra eros odio, a aliquet sapien mollis vel. Duis scelerisque leo nulla, sit amet laoreet mi sodales vitae. Suspendisse vitae consectetur metus, vel imperdiet nulla. Fusce consectetur varius neque, sit amet ornare erat porta ac. Sed ligula lectus, sollicitudin ut pretium quis, maximus eu sapien. Curabitur vitae lacinia urna, sit amet iaculis tellus.",
+		"price_euro": 1.5
+	},{
+		"sku": uuid.uuid4(),
+		"title": "Vanilla icecream",
+		"long_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat semper metus, ac blandit risus viverra eu. Nullam commodo posuere velit, efficitur fringilla turpis posuere a. Vestibulum tempus scelerisque nunc, a efficitur justo hendrerit a. Fusce eu porttitor diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere nec risus nec aliquam. Nulla interdum arcu at sodales vestibulum. Suspendisse velit risus, aliquet vel tristique quis, suscipit sit amet nibh. Vivamus viverra eros odio, a aliquet sapien mollis vel. Duis scelerisque leo nulla, sit amet laoreet mi sodales vitae. Suspendisse vitae consectetur metus, vel imperdiet nulla. Fusce consectetur varius neque, sit amet ornare erat porta ac. Sed ligula lectus, sollicitudin ut pretium quis, maximus eu sapien. Curabitur vitae lacinia urna, sit amet iaculis tellus.",
+		"price_euro": 2.5
+	},{
+		"sku": uuid.uuid4(),
+		"title": "Lemon icecream",
+		"long_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat semper metus, ac blandit risus viverra eu. Nullam commodo posuere velit, efficitur fringilla turpis posuere a. Vestibulum tempus scelerisque nunc, a efficitur justo hendrerit a. Fusce eu porttitor diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere nec risus nec aliquam. Nulla interdum arcu at sodales vestibulum. Suspendisse velit risus, aliquet vel tristique quis, suscipit sit amet nibh. Vivamus viverra eros odio, a aliquet sapien mollis vel. Duis scelerisque leo nulla, sit amet laoreet mi sodales vitae. Suspendisse vitae consectetur metus, vel imperdiet nulla. Fusce consectetur varius neque, sit amet ornare erat porta ac. Sed ligula lectus, sollicitudin ut pretium quis, maximus eu sapien. Curabitur vitae lacinia urna, sit amet iaculis tellus.",
+		"price_euro": 0.5
+	}]
+	return fake_response
+
+def create_product(sku, title, long_description, price_euro):
+	''' Insertar todo esto en una bbdd '''
+	print(f"Crear sku={sku} y title={title}")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
-from flask import jsonify
-from flask import Flask
+from flask import jsonify, request, Flask
+from catalog import get_products, create_product
+
 app = Flask(__name__)
+
+@app.route('/product', methods=['GET', 'POST'])
+def list_all_products():
+	'''This view manages the CRUD of products'''
+	print("Hola mundo")
+	if request.method == 'GET':
+		response = get_products()
+		return jsonify(response)
+	
+	if request.method == 'POST':
+		data = request.get_json()
+		create_product(
+			data['sku'],
+			data['title'],
+			data['long_description'],
+			data['price_euro'])
+		return jsonify({ "status": "ok"})
+
 
 @app.route('/hello')
 def hello_world():
